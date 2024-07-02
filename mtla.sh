d2d421d25390c81bf9433e54fe2920bf0566d45e
mikehundley <mikehundley@aol.com>
2024-07-01 20:37:49 -0400

    Add files via upload

---
+++ mtla.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true
+do
+    # Your command or commands go here
+    echo -en "2.5" > /data/params/d/MapTargetLatA
+    
+    # Wait for 10 seconds before the next iteration
+    sleep 10
+done
