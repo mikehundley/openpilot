320b791039565338ac23e91995b6e6d9c5bb53fb
mikehundley <mikehundley@aol.com>
2024-07-01 20:38:18 -0400

    Update mtla.sh

--- mtla.sh
+++ mtla.sh
@@ -5,6 +5,6 @@ do
     # Your command or commands go here
     echo -en "2.5" > /data/params/d/MapTargetLatA
     
-    # Wait for 10 seconds before the next iteration
-    sleep 10
+    # Wait for 60 seconds before the next iteration
+    sleep 60
 done
