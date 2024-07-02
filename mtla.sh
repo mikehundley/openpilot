0006586a5edcd2f7a676bccc536a2cb9f03bca7a
mikehundley <mikehundley@aol.com>
2024-07-01 20:39:37 -0400

    Update mtla.sh

--- mtla.sh
+++ mtla.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    # Your command or commands go here
+    # Create MapTargetLatA parameter
     echo -en "2.5" > /data/params/d/MapTargetLatA
     
     # Wait for 60 seconds before the next iteration
