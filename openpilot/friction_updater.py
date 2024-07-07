6bf3f9bba16e7a14961b39b8e24d3330a83ee4a3
mikehundley <mikehundley@aol.com>
2024-07-07 12:39:40 -0400

    Create friction_updater.py

---
+++ openpilot/friction_updater.py
@@ -0,0 +1,28 @@
+import capnp
+import os
+import time
+from common.params import Params
+
+# Load the car.capnp schema
+car_capnp = capnp.load(os.path.expanduser('~/openpilot/cereal/car.capnp'))
+
+params = Params()
+
+def update_friction():
+    # Read the current vEgo value
+    car_state = messaging.recv_one(socket)
+    vEgo = car_state.carState.vEgo
+
+    # Determine the appropriate friction value
+    if vEgo > 22.4:  # 50 mph in m/s
+        friction = 0
+    else:
+        friction = 17
+
+    # Update the TorqueFriction parameter
+    params.put("TorqueFriction", str(friction).encode())
+
+# Main loop
+while True:
+    update_friction()
+    time.sleep(0.1)  # Update every 100ms, adjust as needed
