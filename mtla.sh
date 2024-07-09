9d18d3146a07c0e7f5520c6744c1b70e31690da5
mikehundley <mikehundley@aol.com>
2024-07-08 21:28:30 -0400

    Update mtla.sh

--- mtla.sh
+++ mtla.sh
@@ -4,10 +4,6 @@ VALUE="2.8"
 
 while true
 do
-    # Make directory if it doesnt exist
-    # sudo mkdir -p /dev/shm/params/d
-    # sudo mkdir -p /data/params/d
-    
     # Create MapTargetLatA parameter
     echo -en "$VALUE" > /data/params/d/MapTargetLatA
     sudo echo -en "$VALUE" > /dev/shm/params/d/MapTargetLatA
