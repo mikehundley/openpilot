04d67edc91214a2b98edcd03cbbb42ec233e2378
mikehundley <mikehundley@aol.com>
2024-07-08 21:27:25 -0400

    Delete fucheck.py

--- fucheck.py
+++
@@ -1,8 +0,0 @@
-# Check to see if friction_updater.py is running
-if pgrep -f /data/openpilot/openpilot/friction_updater.py > /dev/null; then
-    echo "friction_updater.py is running"
-else
-    echo "friction_updater.py is not running"
-fi
-
-echo "Current Friction = $(cat /data/params/d/TorqueFriction)"
