33179d3986430122295dc208904c2a3eec36209d
mikehundley <mikehundley@aol.com>
2024-07-08 22:57:52 -0400

    Update mtla.sh

--- mtla.sh
+++ mtla.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 VALUE="2.8"
+PARAMS="/data/params/d/MapTargetLatA"
+MEM_PARAMS="/dev/shm/params/d/MapTargetLatA"
+
+write_value() {
+    echo -n "$VALUE" > "$PARAMS"
+    sudo echo -n "$VALUE" > "$MEM_PARAMS"
+   # echo "Value updated to $VALUE"
+}
 
 while true
 do
-    # Create MapTargetLatA parameter
-    echo -en "$VALUE" > /data/params/d/MapTargetLatA
-    sudo echo -en "$VALUE" > /dev/shm/params/d/MapTargetLatA
+    if [ ! -f "$PARAMS" ] || [ ! -f "$MEM_PARAMS" ]; then
+        # File doesn't exist, write the value
+        write_value
+    else
+        # Check if the current content is different from VALUE
+        CURRENT_VALUE=$(cat "$PARAMS")
+        if [ "$CURRENT_VALUE" != "$VALUE" ]; then
+            write_value
+        else
+            :
+        fi
+    fi
     
     # Wait for 60 seconds before the next iteration
     sleep 60
