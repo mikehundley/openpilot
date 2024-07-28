a360ccce607e915b8baf6c146cb4c54dbd380fe1
mikehundley <mikehundley@aol.com>
2024-07-28 13:58:21 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -89,7 +89,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP = [0., 35.]
       ret.lateralTuning.pid.kiV = [0.09, 0.06]
       ret.lateralTuning.pid.kf = 1.1  
-      ret.steerActuatorDelay = 0.08
+      ret.steerActuatorDelay = 0.09
       ret.steerLimitTimer = 0.4
 
     else:
@@ -106,7 +106,7 @@ class CarInterface(CarInterfaceBase):
       if ret.flags & HyundaiFlags.CANFD_CAMERA_SCC and not hda2:
         ret.spFlags |= HyundaiFlagsSP.SP_CAMERA_SCC_LEAD.value
     else:
-      ret.longitudinalTuning.kpV = [0.4]
+      ret.longitudinalTuning.kpV = [0.5]
       ret.longitudinalTuning.kiV = [0.0]
       ret.experimentalLongitudinalAvailable = candidate not in (UNSUPPORTED_LONGITUDINAL_CAR | NON_SCC_CAR)
       if candidate in CAMERA_SCC_CAR:
@@ -116,8 +116,9 @@ class CarInterface(CarInterfaceBase):
 
     ret.stoppingControl = True
     ret.startingState = True
-    ret.vEgoStarting = 0.2
-    ret.startAccel = 1.0
+    ret.vEgoStarting = 0.6
+    ret.vEgoStopping = 0.2
+    ret.startAccel = 1.5
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
