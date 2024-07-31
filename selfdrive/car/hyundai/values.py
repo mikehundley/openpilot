8077ef8fc80e4f836950e51a05623942e2e6b33f
mikehundley <mikehundley@aol.com>
2024-07-30 20:41:33 -0400

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -342,7 +342,7 @@ class CAR(Platforms):
       HyundaiCarDocs("Hyundai Tucson Hybrid 2022-24", "All", car_parts=CarParts.common([CarHarness.hyundai_n])),
       HyundaiCarDocs("Hyundai Tucson Plug-in Hybrid 2024", "All", car_parts=CarParts.common([CarHarness.hyundai_n])),
     ],
-    CarSpecs(mass=1630, wheelbase=2.756, steerRatio=TUCSON_4G_SR, tireStiffnessFactor=0.385),
+    CarSpecs(mass=1630, wheelbase=2.756, steerRatio=TUCSON_4G_SR, tireStiffnessFactor=1.0),
   )
   HYUNDAI_SANTA_CRUZ_1ST_GEN = HyundaiCanFDPlatformConfig(
     [HyundaiCarDocs("Hyundai Santa Cruz 2022-24", car_parts=CarParts.common([CarHarness.hyundai_n]))],
