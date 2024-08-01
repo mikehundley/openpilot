61d6d1b72bf360f508c11e3b3892105bf2fde1ce
mikehundley <mikehundley@aol.com>
2024-07-31 20:33:14 -0400

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -74,11 +74,11 @@ def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
     return 1.75
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    return 1.45
+    return 1.4
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 1.25
+    return 1.1
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 1.0
+    return 0.75
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
