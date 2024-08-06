340013d6cdb46a21b9c006d5cf6292b79c3bfb1f
mikehundley <mikehundley@aol.com>
2024-08-06 16:35:57 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -84,16 +84,16 @@ class CarInterface(CarInterfaceBase):
 
     # *** lateral control ***
     if candidate in CANFD_CAR:
- #     ret.lateralTuning.pid.kpBP = [0., 17.5, 35.]
- #     ret.lateralTuning.pid.kpV = [0.95, 0.8, 0.6]
+      ret.lateralTuning.pid.kpBP = [0., 17.5, 35.]
+      ret.lateralTuning.pid.kpV = [1.0, 0.8, 0.6]
       ret.lateralTuning.pid.kiBP = [0., 22.5, 31.3]
-      ret.lateralTuning.pid.kiV = [0.07, 0.05, 0.01]
+      ret.lateralTuning.pid.kiV = [0.1, 0.05, 0.01]
       ret.lateralTuning.pid.kdBP = [0., 35.]
       ret.lateralTuning.pid.kdV = [0.05, 0.01]
       ret.lateralTuning.pid.kfBP = [23.1, 33.1]
       ret.lateralTuning.pid.kfV = [1.1, 0.3] 
      # ret.lateralTuning.pid.kf = 1.1
-      ret.steerActuatorDelay = 0.09
+      ret.steerActuatorDelay = 0.095
       ret.steerLimitTimer = 0.4
       
     else:
